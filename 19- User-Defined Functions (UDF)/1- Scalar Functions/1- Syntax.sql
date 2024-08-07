552b618038a20bffbb803cd49c232210fb8b9cd9
Saad Salman <saadsalmanakram1@gmail.com>
2024-08-08 01:59:10 +0500

    Create 1- Syntax.sql

---
+++ 19- User-Defined Functions (UDF)/1- Scalar Functions/1- Syntax.sql
@@ -0,0 +1,8 @@
+CREATE FUNCTION function_name (@parameter datatype, ...)
+RETURNS return_datatype
+AS
+BEGIN
+    DECLARE @result return_datatype;
+    -- Function logic goes here
+    RETURN @result;
+END;
