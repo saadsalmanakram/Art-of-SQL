1395812dd1c7b2120caa330c1bbccc27923e0bbc
Saad Salman <saadsalmanakram1@gmail.com>
2024-08-07 07:02:42 +0500

    Create 1- Basic Example.sql

---
+++ 4- Tables/1- `CREATE TABLE`/1- Basic Example.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    BirthDate DATE,
+    HireDate DATE,
+    Salary DECIMAL(10, 2)
+);
