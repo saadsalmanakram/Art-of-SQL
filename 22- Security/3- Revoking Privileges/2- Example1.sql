81979d3ac36da4edf8ddc7bd4fcdff6d58ecc4bb
Saad Salman <saadsalmanakram1@gmail.com>
2024-08-08 13:35:07 +0500

    Create 2- Example1.sql

---
+++ 22- Security/3- Revoking Privileges/2- Example1.sql
@@ -0,0 +1,5 @@
+-- Revoke INSERT privilege on the 'employees' table from user 'johndoe'
+REVOKE INSERT ON employees FROM johndoe;
+
+-- Revoke ALL privileges on the 'departments' table from user 'janedoe'
+REVOKE ALL PRIVILEGES ON departments FROM janedoe;
