0a5d9ec11187ae25b80b0bffa7bd81c25afd3cca
Saad Salman <saadsalmanakram1@gmail.com>
2024-08-08 13:36:55 +0500

    Create 2- Example1.sql

---
+++ 22- Security/4- Creating and Managing Users/2- Example1.sql
@@ -0,0 +1,5 @@
+-- Create a new user 'johndoe' with password 'password123'
+CREATE USER 'johndoe'@'localhost' IDENTIFIED BY 'password123';
+
+-- Drop the user 'johndoe'
+DROP USER 'johndoe'@'localhost';
